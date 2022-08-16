5bb8a5ff5b053e1624be50ccef7540b4ca1f1d70
hlheo1011 <hoh1011@naver.com>
2022-08-16 17:23:06 +0900

    2022-08-16 pm3

--- javastudy/11_NETWORK/src/ex04_socket/ServerMain.java
+++ javastudy/11_NETWORK/src/ex04_socket/ServerMain.java
@@ -2,6 +2,7 @@ package ex04_socket;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -32,12 +33,27 @@ public class ServerMain {
 				Socket clientSocket = serverSocket.accept();
 				clientCnt++;
 				
-				// 클라이언트에게 "Welcome"메세지 전송
-				// 바이트 출력 스트림 중에서 DataOutputStream은 writeUTF 메소드를 이용해서
-				// 한글을 깨짐없이 보낼 수 있다.
+				// 클라이언트에게 Welcome 메시지 전송
+				// 바이트 출력 스트림 중에서 DataOutputStream은 writeUTF() 메소드를 이용해서
+				// 한글을 깨짐 없이 보낼 수 있다.
 				DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-				out.writeUTF("[서버] 게스트" + clientCnt + "님 환영합니다");
+				out.writeUTF("[서버] 게스트" + clientCnt + "님 환영합니다!");
+				
+				// 클라이언트가 보낸 메시지 확인
+				// 클라이언트가 OutputStreamWriter의 write()로 보냈으므로
+				// InputStreamReader의 read()로 확인
+				InputStreamReader in = new InputStreamReader(clientSocket.getInputStream());
+				char[] cbuf = new char[5];
+				int readCnt = 0;
+				StringBuilder sb = new StringBuilder();
+				while((readCnt = in.read(cbuf)) != -1) {
+					sb.append(cbuf, 0, readCnt);
+				}
+				System.out.println("[서버] 클라이언트가 보낸 메시지 : " + sb.toString());
+				
+				// 입출력 스트림 종료
 				out.close();
+				in.close();
 				
 				// 클라이언트 접속 종료
 				clientSocket.close();
@@ -49,9 +65,9 @@ public class ServerMain {
 					break;
 				}
 				
-			} // while(true)
+			}  // while(true)
 			
-		} catch (IOException e) {
+		} catch(IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
