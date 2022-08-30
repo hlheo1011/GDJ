a0020e1d4ae1e5d3d2b89d9b5989105a56046454
hlheo1011 <hoh1011@naver.com>
2022-08-30 15:15:09 +0900

    2022-08-30pm1

--- dbstudy/18_DQL그룹(HR).sql
+++ dbstudy/18_DQL그룹(HR).sql
@@ -1,6 +1,6 @@
       -- 그룹(GROUP BY)
 -- 1. GROUP BY절에서 지정한 칼럼의 데이터는 하나로 모아서 한 번만 조회가 됨
--- 2. SELECT절에서 조회할 칼럼은 "반드시"
+-- 2. SELECT절에서 조회할 칼럼은 "반드시" GROUP BY절에 존재해야 함
 
 -- EMPLOYEES 테이블
 
@@ -59,10 +59,11 @@ HAVING
 -- WHERE 절과 HAVING절이 고민되면 고민하지말고 WHERE절에서 써라.
 -- 불필요한걸 미리 뺴고(WHERE)
 
--- 2) 소속된 사원수가 10명 이상이 부서의 연봉평균 조회하기(HAVING절만 가능)
+-- 2) 소속된 사원수가 10명 이상인 부서의 연봉평균 조회하기(HAVING절만 가능)
 -- 소속된 사원수는 GROUP BY 이후에만 알 수 있기 때문에 WHERE절로 처리가 불가능
-SELECT30
+SELECT
        DEPARTMENT_ID
+     , COUNT(*) AS 부서별사원수
      , FLOOR(AVG(SALARY)) AS 부서별연봉평균
   FROM
        EMPLOYEES
@@ -71,4 +72,68 @@ SELECT30
 HAVING
        COUNT(*) >= 10;
 
+
+-- 연습.
+
+-- 1. 급여평균이 10000 이상인 부서의 부서번호와 급여평균을 조회하기
+SELECT
+       DEPARTMENT_ID, FLOOR(AVG(SALARY))
+  FROM EMPLOYEES
+ GROUP BY DEPARTMENT_ID
+HAVING AVG(SALARY) >= 10000;
+
+
+-- 2. 동일한 부서번호(DEPARTMENT_ID)로 그룹화하기
+--    동일한 부서번호를 가진 사원들을 직업아이디(JOB_ID)로 다시 그룹화하기
+--    즉, 부서별 직업아이디별로 그룹화하기
+--    부서별 직업아이디별로 그룹화하여 각 그룹의 사원수 조회하기
+--    부서번호가 없는 사원은 제외하기
+SELECT DEPARTMENT_ID, JOB_ID, COUNT(*) AS 사원수
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID IS NOT NULL
+ GROUP BY DEPARTMENT_ID, JOB_ID
+ ORDER BY 사원수;
  
+-- DEPARTMENTS 테이블 연습.
+
+-- 1. 동일한 지역(LOCATION_ID)으로 그룹화하여 조회하기
+SELECT LOCATION_ID
+  FROM DEPARTMENTS
+ GROUP BY LOCATION_ID;
+
+
+-- 2. 동일한 지역(LOCATION_ID)으로 그룹화하여 각 지역별 존재하는 부서수 조회하기
+--    부서수가 2 이상인 지역만 조회하기 (HAVING COUNT(*) >= 2;
+SELECT LOCATION_ID, COUNT(*)
+  FROM DEPARTMENTS
+ GROUP BY LOCATION_ID
+HAVING COUNT(*) >= 2;
+
+
+-- 3. 동일한 지역(LOCATION_ID)으로 그룹화하여 각 지역별 존재하는 부서수 조회하기
+--    MANAGER_ID가 없는 지역은 제외하고 조회하기
+SELECT LOCATION_ID, COUNT(*)
+  FROM DEPARTMENTS
+ WHERE MANAGER_ID IS NOT NULL
+ GROUP BY LOCATION_ID;
+
+-- 4. 부서명(DEPARTMENT_NAME)의 첫 2글자로 그룹화하여 해당 그룹의 개수 조회하기
+SELECT SUBSTR(DEPARTMENT_NAME, 1, 2), COUNT(*)
+  FROM DEPARTMENTS
+ GROUP BY SUBSTR(DEPARTMENT_NAME, 1, 2);
+
+
+-- 5. 부서명(DEPARTMENT_NAME)의 첫 2글자로 그룹화하여 해당 그룹의 개수 조회하기
+--    부서명의 첫 2글자가 'It', 'Co'인 부서만 조회하기
+-- ★ 조건이 나오면 WHERE절을 써라
+SELECT SUBSTR(DEPARTMENT_NAME, 1, 2), COUNT(*)
+  FROM DEPARTMENTS
+ WHERE SUBSTR(DEPARTMENT_NAME, 1, 2) = 'IT'
+    OR SUBSTR(DEPARTMENT_NAME, 1, 2) = 'Co'
+ GROUP BY SUBSTR(DEPARTMENT_NAME, 1, 2);
+ 
+-- 위보다 좋은 코드
+SELECT SUBSTR(DEPARTMENT_NAME, 1, 2), COUNT(*)
+  FROM DEPARTMENTS
+ WHERE SUBSTR(DEPARTMENT_NAME, 1, 2) IN('IT', 'Co')
+ GROUP BY SUBSTR(DEPARTMENT_NAME, 1, 2);
