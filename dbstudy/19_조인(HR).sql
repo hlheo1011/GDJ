b91af658794c646438d90b24bd8149c49e5cffe8
hlheo1011 <hoh1011@naver.com>
2022-08-31 14:04:20 +0900

    2022-08-31am2

--- dbstudy/19_조인(HR).sql
+++ dbstudy/19_조인(HR).sql
@@ -95,11 +95,37 @@ SELECT E.EMPLOYEE_ID, E.LAST_NAME, D.DEPARTMENT_NAME, L.CITY, C.COUNTRY_NAME
 
 -- 7. 모든 사원들의 EMPLOYEE_ID, LAST_NAME, DEPARTMENT_NAME을 조회하시오.
 --    부서번호(DEPARTMENT_ID)가 없는 사원도 조회하고, EMPLOYEE_ID순으로 오름차순 정렬하시오.
---    부서번호(DEPARTMENT_ID)가 없는 사원의 부서명(DEPARTMENT_NAME)은 'None'으로 조회하시오.
+--    부서번호(DEPARTMENT_ID)가 없는 사원의 부서명(DEPARTMENT_NAME)은 'None'으로 조회하시오
+
+-- 부서               - 사원
+-- 일치하는정보포함(+)   모든정보포함
+
+-- 오른쪽에 있는 사원 테이블의 모든 정보 포함을 위하여 오른쪽 외부 조인
+
+SELECT E.EMPLOYEE_ID, E.LAST_NAME, NVL(D.DEPARTMENT_NAME, 'None')
+  FROM DEPARTMENTS D RIGHT OUTER JOIN EMPLOYEES E
+    ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+ ORDER BY E.EMPLOYEE_ID;
+ 
+SELECT E.EMPLOYEE_ID, E.LAST_NAME, NVL(D.DEPARTMENT_NAME, 'None')
+  FROM DEPARTMENTS D, EMPLOYEES E
+ WHERE D.DEPARTMENT_ID(+) = E.DEPARTMENT_ID
+ ORDER BY E.EMPLOYEE_ID;
+ 
+    
 
 
 -- 8. 부서별 근무하는 사원수를 조회하시오.
 --    단, 근무하는 사원이 없으면 0으로 조회하시오.
+SELECT D.DEPARTMENT_ID, D.DEPARTMENT_NAME, COUNT(E.DEPARTMENT_ID) AS 사원수
+  FROM DEPARTMENTS D LEFT OUTER JOIN EMPLOYEES E
+    ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+ GROUP BY D.DEPARTMENT_ID, D.DEPARTMENT_NAME;
+
+SELECT D.DEPARTMENT_ID, D.DEPARTMENT_NAME, COUNT(E.DEPARTMENT_ID) AS 사원수
+  FROM DEPARTMENTS D, EMPLOYEES E
+ WHERE D.DEPARTMENT_ID = E.DEPARTMENT_ID(+)
+ GROUP BY D.DEPARTMENT_ID, D.DEPARTMENT_NAME;
 
 
 
@@ -109,6 +135,57 @@ SELECT E.EMPLOYEE_ID, E.LAST_NAME, D.DEPARTMENT_NAME, L.CITY, C.COUNTRY_NAME
 -- 9. MANAGER보다 먼저 입사한 사원들의 EMPLOYEE_ID, LAST_NAME, HIRE_DATE과 MANAGER의 HIRE_DATE를 조회하시오.
 --    사원의 HIRE_DATE가 MANAGER의 HIRE_DATE보다 작은 사원을 조회하시오. (MANAGER보다 먼저 입사한 사원)
 
+-- 비교 대상
+-- 1) 모든 사원 정보 : EMPLOYEES E
+-- 2) MANAGER의 정보 : EMPLOYEES M
+
+-- 관계
+-- PK           FK
+-- EMPLOYEE_ID  MANAGER_ID
+
+-- 조인조건
+-- 사원들의 매니저번호 = 매니저의 사원번호
+-- E.MANAGER_ID = M.EMPLOYEE_ID
+
+SELECT
+       E.EMPLOYEE_ID -- 사원번호
+     , E.LAST_NAME   -- 사원명
+     , E.HIRE_DATE   -- 사원입사일
+     , M.LAST_NAME   -- 상사이름
+     , M.HIRE_DATE   -- 상사입사일
+  FROM
+       EMPLOYEES E INNER JOIN EMPLOYEES M --  SELFJOIN
+    ON 
+       E.MANAGER_ID = M.EMPLOYEE_ID
+ WHERE
+       TO_DATE(E.HIRE_DATE) < TO_DATE(M.HIRE_DATE);
+
 
 -- 10. 같은 부서의 사원들 중에서 나보다 늦게 입사하였으나 연봉을 더 많이 받는 사원이 있는 사원들의
 --     DEPARTMENT_ID, LAST_NAME, SALARY, HIRE_DATE와 높은 연봉을 받는 사원의 LAST_NAME, SALARY, HIRE_DATE를 조회하시오.
+
+-- 비교 대상
+-- 1) 나 : EMPLOYEES ME
+-- 2) 남 : EMPLOYEES YOU
+
+-- 조인조건
+-- ME.DEPARTMENT_ID = YOU.DEPARTMENT_ID
+
+SELECT
+       ME.DEPARTMENT_ID  -- 내 부서번호
+     , ME.LAST_NAME      -- 내 이름
+     , ME.SALARY         -- 내 급여
+     , ME.HIRE_DATE      -- 내 입사일
+     , YOU.DEPARTMENT_ID -- 남 부서번호
+     , YOU.LAST_NAME     -- 남 이름
+     , YOU.SALARY        -- 남 급여
+     , YOU.HIRE_DATE     -- 남 입사일
+  FROM
+       EMPLOYEES ME INNER JOIN EMPLOYEES YOU
+    ON
+       ME.DEPARTMENT_ID = YOU.DEPARTMENT_ID
+ WHERE
+       TO_DATE(ME.HIRE_DATE) < TO_DATE(YOU.HIRE_DATE)
+   AND
+       ME.SALARY < YOU.SALARY;
+     
