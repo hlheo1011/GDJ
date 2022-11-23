86b91aae18643491cc0e2e4ba22bf9cfa526a878
hyolim <hoh1011@naver.com>
2022-11-23 21:01:34 +0900

    2022-11-23

---
+++ springstudy/15_Pjt_Blog/src/main/java/com/gdu/app15/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.gdu.app15.service;
+
+import org.springframework.ui.Model;
+
+public interface CommentService {
+	
+	
+	
+	
+}
---
+++ springstudy/15_Pjt_Blog/src/main/java/com/gdu/app15/service/CommentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.gdu.app15.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.gdu.app15.mapper.BlogMapper;
+import com.gdu.app15.util.PageUtil;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+	
+
+	
+	
+}
